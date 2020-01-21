e5f39f357ec6f903f9b83bb215a34f19181d8490
defending1 <work.albertodefendi@gmail.com>
2020-01-21 13:39:20 +0100

    Added formbox

---
+++ src/components/Formbox/actions.js
@@ -0,0 +1,4 @@
+export const ADD_CREDENTIALS = ({
+  ssid: '',
+  password: ''
+})
---
+++ src/components/Formbox/reducer.js
@@ -0,0 +1,12 @@
+
+export const CREDENTIALS = (state = {
+  ssid: '',
+  password: ''
+}, action) => {
+  switch (action.type) {
+    case 'ADD_CREDENTIALS':
+      return action.state;
+    default:
+      return state;
+  }
+};
