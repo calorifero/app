da19a108436307dc368c82ffa27c7460aff091fc
defending1 <work.albertodefendi@gmail.com>
2020-01-21 20:54:07 +0100

    Removed formbox

--- src/components/FormBox/index.js
+++ src/components/FormBox/index.js
@@ -1,33 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, TextInput, View } from "react-native";
 import { Formik } from "formik";
 import { mapStateToProps } from "../../redux/mapStateToProps";
 import { useSelector, useDispatch } from "react-redux";
 import { ADD_CREDENTIALS } from "./actions";
+import WifiManager from "react-native-wifi";
+
+const getSSID = () => {
+  WifiManager.getCurrentWifiSSID().then(
+    ssid => {
+      console.log("Your current connected wifi SSID is " + ssid);
+    },
+    () => {
+      console.log("Cannot get current SSID!");
+    }
+  );
+};
 
 export const FormBox = props => {
   const credentials = useSelector(state => state.credentials);
   const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <Formik
-      initialValues={{ ssid: "", password: "" }}
-      onSubmit={values => dispatch(ADD_CREDENTIALS({ values }))}
-    >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
-          <TextInput
-            onChangeText={handleChange("ssid")}
-            onBlur={handleBlur("ssid")}
-            value={values.ssid}
-          />
-          <TextInput
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            value={values.password}
-          />
-          <Button onPress={handleSubmit} title="Submit" />
-        </View>
-      )}
-    </Formik>
+    <View>
+      <TextInput
+        type="username"
+        placeholder="SSID"
+        onChangeText={handleChange("ssid")}
+        onBlur={handleBlur("ssid")}
+        value={values.ssid}
+      />
+      <TextInput
+        type="password"
+        placeholder="Password"
+        onChangeText={handleChange("password")}
+        onBlur={handleBlur("password")}
+        value={values.password}
+      />
+      <Button onPress={handleSubmit} title="Submit" />
+    </View>
   );
 };
