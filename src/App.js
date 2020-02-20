5e195052921b60a9194f6c3febbbdf5cfa8f1312
defending1 <work.albertodefendi@gmail.com>
2020-02-20 08:58:08 +0100

    Added android notch support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, StatusBar, View, SafeAreaView } from "react-native";
 import { registerRootComponent } from "expo";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
@@ -8,7 +8,7 @@ import { Home } from "./components/Home/index";
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Home />
       </SafeAreaView>
     </Provider>
