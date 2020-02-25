906879294ff62e4202fa4fc495621ac9cc7074b2
root <root@Asus.localdomain>
2020-02-25 20:20:53 +0000

    Added react svg

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,24 +8,12 @@ import {
   StrongMessage,
   ModelContainer
 } from "./style";
+import { Design } from './Design/index'
 
 export const Home = () => {
   return (
     <Container>
-      <RightMessage>
-        <MessageText>Calòr, qual'è la temperatura di casa?</MessageText>
-      </RightMessage>
-      <ModelContainer>
-      <LeftMessage>
-        <MessageText>Questa settimana ho registrato 18 gradi</MessageText>
-      </LeftMessage>
-      <LeftMessage>
-        <StrongMessage>La temperatura è perfetta</StrongMessage>
-        <MessageText>
-          Fino a ora hai risparmiato 100€ Continua così!
-        </MessageText>
-      </LeftMessage>
-      </ModelContainer>
+      <Design />
     </Container>
   );
 };
