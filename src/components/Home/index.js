1b3932776d9c299605577827b8440c293080975d
defending1 <work.albertodefendi@gmail.com>
2020-03-01 18:17:39 +0000

    .

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React from 'react';
+import { Text } from 'react-native';
 import {
   Container,
+  Header,
+  Description,
   Logo,
   LeftMessage,
   MessageText,
   RightMessage,
   StrongMessage,
   ModelContainer
-} from "./style";
-import { Design } from './Design/index'
+} from './style';
+import { Design } from './Design/index';
 
 export const Home = () => {
   return (
     <Container>
       <Design />
+      <Header>Lorem</Header>
+      <Description>Ipsum dolor</Description>
     </Container>
   );
 };
--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -1,40 +1,41 @@
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
-} from "react-native-responsive-screen";
+} from 'react-native-responsive-screen';
 
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
+
 export const Container = styled.ScrollView`
   flex: 1;
 `;
 
 export const Logo = styled.Text`
-  font-size: ${hp("15%")};
+  font-size: ${hp('15%')};
   text-align: center;
 `;
 
 export const MessageText = styled.Text`
-  font-size: ${hp("2.5%")};
-  lineHeight: 20;
+  font-size: ${hp('2.5%')};
+  lineheight: 20;
 `;
 
 export const LeftMessage = styled.View`
-  border-radius: ${hp("4%")};
-  background-color: #AAFFA0;
+  border-radius: ${hp('4%')};
+  background-color: #aaffa0;
   max-width: 80%;
-  marginTop: 5;
-  marginBottom: 5;
-  marginLeft: 10;
-  marginRight: 10;
-  paddingLeft: 12;
-  paddingRight: 12;
-  paddingTop: 12;
-  paddingBottom: 12;
+  margintop: 5;
+  marginbottom: 5;
+  marginleft: 10;
+  marginright: 10;
+  paddingleft: 12;
+  paddingright: 12;
+  paddingtop: 12;
+  paddingbottom: 12;
 `;
 
 export const RightMessage = styled(LeftMessage)`
-  background-color: #F4FFBC;
-  alignSelf: flex-end;
+  background-color: #f4ffbc;
+  alignself: flex-end;
 `;
 
 export const StrongMessage = styled(MessageText)`
@@ -43,5 +44,15 @@ export const StrongMessage = styled(MessageText)`
 
 export const ModelContainer = styled.View`
   background-color: black;
-  marginTop: 50;
+  margintop: 50;
+`;
+
+export const Header = styled.Text`
+  text-align: center;
+  font-size: ${hp('8%')};
+`;
+export const Description = styled.Text`
+  font-size: ${hp('2.5%')};
+
+  text-align: center;
 `;
